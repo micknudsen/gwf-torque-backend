4c2fddd240af7798d519da3338849b9f3025d963
Michael Knudsen <michaelk@clin.au.dk>
2022-07-26 16:13:11 +0200

    Implemeted parse_queue_output method

--- src/gwf_torque_backend/torque.py
+++ src/gwf_torque_backend/torque.py
@@ -1,4 +1,6 @@
-from gwf.backends.base import PbsLikeBackendBase
+from xml.etree import ElementTree
+
+from gwf.backends.base import PbsLikeBackendBase, Status
 from gwf.backends.exceptions import BackendError
 from gwf.backends.utils import call
 
@@ -10,3 +12,18 @@ class TorqueBackend(PbsLikeBackendBase):
     @retry(on_exc=BackendError)
     def call_queue_command(self):
         return call('qstat', '-f', '--xml')
+
+    def parse_queue_output(self, stdout):
+        job_states = {}
+        root = ElementTree.fromstring(stdout)
+        for job in root.iter('Job'):
+            job_id = job.find('Job_Id').text
+            state = job.find('job_state').text
+            if state == 'R':
+                job_state = Status.RUNNING
+            elif state in ['H', 'Q', 'T', 'W']:
+                job_state = Status.SUBMITTED
+            else:
+                job_state = Status.UNKNOWN
+            job_states[job_id] = job_state
+        return job_states
