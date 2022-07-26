eda824113f383e36643106aa2a99cc286ab405dd
Michael Knudsen <michaelk@clin.au.dk>
2022-07-26 15:32:58 +0200

    Implemented call_queue_command method

--- src/gwf_torque_backend/torque.py
+++ src/gwf_torque_backend/torque.py
@@ -1,5 +1,12 @@
 from gwf.backends.base import PbsLikeBackendBase
+from gwf.backends.exceptions import BackendError
+from gwf.backends.utils import call
+
+from gwf.utils import retry
 
 
 class TorqueBackend(PbsLikeBackendBase):
-    pass
+
+    @retry(on_exc=BackendError)
+    def call_queue_command(self):
+        return call('qstat', '-f', '--xml')
